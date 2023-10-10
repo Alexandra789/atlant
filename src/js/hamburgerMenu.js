a426f871d894bab21b51e30804ba51aea2386bc5
Alexandra789 <zakovinko99@gmail.com>
2023-10-10 13:56:40 +0300

    implement scroll ban when mobile menu is open

--- src/js/hamburgerMenu.js
+++ src/js/hamburgerMenu.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector(".hamburger__btn");
 const navMenu = document.querySelector(".hamburger__menu");
 const menuLinks = document.querySelectorAll(".hamburger__menu .menu__list-link");
 const btn = document.querySelectorAll('.hamburger .header__buy');
+const body = document.querySelector("body");
 const menuItems = [...menuLinks, ...btn];
 
 menuBtn.addEventListener("click", toggleMobileMenu);
@@ -13,4 +14,5 @@ menuItems.forEach(menuItem => {
 function toggleMobileMenu() {
 	menuBtn.classList.toggle("active");
 	navMenu.classList.toggle("active");
+	menuBtn.classList.contains("active") ? body.style.overflow = 'hidden' : body.style.overflow = 'auto';
 }
